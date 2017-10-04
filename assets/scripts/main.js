a3eaecefef767070619b240b617ba84b4c2f5d01
Michael Estok <mestok@nevermike.local>
2017-10-04 09:44:27 -0400

    fix up code, and making a bad comment

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,1 +1,11 @@
+// post blow up image - need to change inline css to classes
 
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 200) {
+        $(".post-header__img").addClass("scrolled-up").next().addClass("title-space");
+    } else {
+        $(".post-header__img").removeClass("scrolled-up").next().removeClass("title-space");
+    }
+});
