77eb14dccf6c55ec4ed24b7a9ba846f4448ec128
Michael Estok <mestok@nevermike.local>
2018-01-26 14:28:57 -0500

    2018 site base, finally

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,11 +1,26 @@
 // post blow up image - need to change inline css to classes
 
-$(window).scroll(function() {
-    var scroll = $(window).scrollTop();
+// $(window).scroll(function() {
+//     var scroll = $(window).scrollTop();
+//
+//     if (scroll >= 200) {
+//         $(".post-header__img").addClass("scrolled-up").next().addClass("title-space");
+//     } else {
+//         $(".post-header__img").removeClass("scrolled-up").next().removeClass("title-space");
+//     }
+// });
 
-    if (scroll >= 200) {
-        $(".post-header__img").addClass("scrolled-up").next().addClass("title-space");
-    } else {
-        $(".post-header__img").removeClass("scrolled-up").next().removeClass("title-space");
-    }
-});
+// scrolling animations
+
+jQuery('.tourpoint__image').addClass("hidden").viewportChecker({
+    classToAdd: 'visible animated fadeIn',
+    offset: 200
+   });
+
+jQuery('.tourpoint__copy').addClass("hidden").viewportChecker({
+    classToAdd: 'visible animated bounceInRight',
+     offset: 100
+    });
+
+// other animations
+$('.fadeinload').addClass('animated fadeIn');
